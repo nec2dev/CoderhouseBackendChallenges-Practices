3ba7c47e03d8b7d7c5304758f581ce3c69667c31
Nahuel Correa <nahuelezequielcorrea@gmail.com>
2022-07-04 14:53:37 -0300

    Challenge Deliverable 003 OK

--- Class006/challengeDeliverable003/contenedor.js
+++ Class006/challengeDeliverable003/contenedor.js
@@ -1,41 +1,48 @@
 const fs = require("fs");
 class Contenedor {
-  constructor(nombre) {
-    this.nombre = nombre;
-    this.countID = 0;
-    this.contenido = [];
-  }
-
-  async init() {
-    try {
-      let data = await fs.promises.readFile(this.nombre);
-      this.contenido = JSON.parse(data);
-      for (const elemento of this.contenido) {
-        if (elemento.id > this.countID) this.countID = elemento.id;
-      }
-    } catch (error) {
-      console.log("No existe ningún archivo");
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.countID = 0;
+        this.contenido = [];
     }
-  }
 
-  /*getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.*/
-  getById(id) {
-    let resultado;
-    if (this.contenido.length > 0) {
-      resultado = this.contenido.find((objeto) => objeto.id === id);
-      if (resultado === undefined) {
-        resultado = null;
-      }
-    } else {
-      result = "El archivo está vacío";
+    async init() {
+        try {
+            let data = await fs.promises.readFile(this.nombre);
+            this.contenido = JSON.parse(data);
+            for (const elemento of this.contenido) {
+                if (elemento.id > this.countID) this.countID = elemento.id;
+            }
+        } catch (error) {
+            console.log("No existe ningún archivo");
+        }
     }
-    return resultado;
-  }
 
-  /*getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.*/
-  async getAll() {
-    return this.contenido;
-  }
+    /*getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.*/
+    getById(id) {
+        let resultado;
+        if (this.contenido.length > 0) {
+            resultado = this.contenido.find((objeto) => objeto.id === id);
+            if (resultado === undefined) {
+                resultado = null;
+            }
+        } else {
+            result = "El archivo está vacío";
+        }
+        return resultado;
+    }
+
+    /*getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.*/
+    getAll = async() => {
+        try {
+            const productos = await fs.promises.readFile(`${this.nombre}`, 'utf-8');
+            return JSON.parse(productos);
+        } catch (error) {
+            console.log('El archivo est{a vacío');
+            await fs.promises.readFile(this.nombre, 'utf-8');
+            return productos
+        }
+    }
 
 }
 
--- Class006/challengeDeliverable003/index.js
+++ Class006/challengeDeliverable003/index.js
@@ -10,29 +10,28 @@ const Contenedor = require("./contenedor");
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const PRODUCTOS = new Contenedor("./productos.txt")
+const PRODUCTOS = new Contenedor("productos.txt")
 
 PRODUCTOS.init();
 app.listen(PORT);
 
 app.on("error", (error) => {
-  console.log("Error");
+    console.log("Error");
 });
 
 app.get("/", (req, res) => {
-  res.send('<h1 style=color:blue;>Bienvenidos al Servidor Express</h1>');
+    res.send('<h1 style=color:blue;>Bienvenidos al Servidor Express</h1>');
 });
 
 /*Ruta get '/productos' que devuelva un array con todos los productos disponibles en 
 el servidor*/
-app.get("/productos", (req, res) => {
-  res.send(PRODUCTOS.getAll());
+app.get("/productos", async(req, res) => {
+    let productos = await PRODUCTOS.getAll();
+    res.send(productos);
 });
 
 /*Ruta get '/productoRandom' que devuelva un producto elegido al azar entre todos 
 los productos disponibles*/
 app.get("/productoRandom", (req, res) => {
-  res.send(PRODUCTOS.getById(Math.floor(Math.random()*(PRODUCTOS.countID-1+1)+1)));
+    res.send(PRODUCTOS.getById(Math.floor(Math.random() * (PRODUCTOS.countID - 1 + 1) + 1)));
 });
-
-
