b980384025422c885d36758bae650d861e65b0dd
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-07-24 20:54:18 -0300

    All OK

--- Class008/challengeDeliverable004/index.js
+++ Class008/challengeDeliverable004/index.js
@@ -1,12 +1,11 @@
 const express = require ('express');
 const app = express();
-const port = 8080;
+const port = 8088;
 const routes = require('./router/routes.js');
 const serverExpress = app.listen(port, () => {
     console.log(`Server running on port: ${serverExpress.address().port}`);
 });
 
-app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('view'));
 app.use('/api/productos', routes.allProducts);
@@ -15,4 +14,5 @@ app.use('/api/productos', routes.newProduct);
 app.use('/api/productos', routes.newProductHtml);
 app.use('/api/productos', routes.updateProduct);
 app.use('/api/productos', routes.deleteProduct);
-serverExpress.on('error', (error) => {`Error server: ${error}`});
+
+serverExpress.on('error', (error) => {`Error eserver: ${error}`});
--- Class008/challengeDeliverable004/model/model.js
+++ Class008/challengeDeliverable004/model/model.js
@@ -1,46 +1,33 @@
 const fs = require("fs").promises;
-const pathFile = `Class008/challengeDeliverable004/model/productos.json`;
+const pathFile = `./model/productos.json`;
 const encoding = `utf-8`;
 
 class Contenedor {
-  constructor(fs, pathFile, encoding) {
+  constructor(fs, pathFile, encoding) { 
     this.fs = fs;
     this.pathFile = pathFile;
     this.encoding = encoding;
   }
-
-  async init() {
-    try {
-      let data = await fs.promises.readFile(this.nombre);
-      this.contenido = JSON.parse(data);
-      for (const elemento of this.contenido) {
-        if (elemento.id > this.countID) this.countID = elemento.id;
-      }
-    } catch (error) {
-      console.log("No existe ningún archivo");
-    }
-  }
-
+  
   async validarArchivo() {
     try {
-      let datos = await this.fs.readFile(this.pathFile, this.encoding);
-      if (!datos) {
-        try {
-          this.fs.writeFile(this.pathFile, "");
-        } catch (error) {
-          console.log(
-            `Se produjo un error al intentar inicializar el archivo: ${error}`
-          );
+      let datos = await this.fs.readFile(this.pathFile, this.encoding); 
+        if (!datos) {
+          try{
+            this.fs.writeFile(this.pathFile, '');
+          } 
+          catch(error) {
+            console.log(`Se produjo un error al intentar inicializar el archivo: ${error}`);
+          }
         }
-      }
-      return true;
-    } catch (error) {
+        return true;
+    }catch (error) {
       console.log(`Se produjo un error al intentar leer el archivo: ${error}`);
-      try {
-        this.fs.writeFile(this.pathFile, "");
+      try{
+        this.fs.writeFile(this.pathFile, '');
         console.log(`Se creó exitosamente el archivo`);
         return true;
-      } catch (e) {
+      }catch(e){
         console.log(`Se produjo un error al intentar crear el archivo: ${e}`);
         return false;
       }
@@ -49,30 +36,28 @@ class Contenedor {
 
   async getAll() {
     try {
-      let productos = await this.fs.readFile(this.pathFile, this.encoding);
-      if (productos) {
-        const arrayProductos = JSON.parse(productos);
-        return arrayProductos;
+      let productos = await this.fs.readFile(this.pathFile, this.encoding); 
+      if (productos){
+        const arrayProductos = JSON.parse(productos); 
+        return (arrayProductos);
       }
-      return [];
+      return ([]);
     } catch (error) {
       console.log(`Error al leer el archivo en el método getAll(): ${error}`);
     }
   }
 
   async getById(id) {
-    if (!isNaN(id)) {
+    if(!isNaN(id)){
       try {
         let arrayProductos = await this.getAll();
-        if (arrayProductos.length > 0) {
+        if (arrayProductos.length > 0){
           let product = arrayProductos.find((producto) => producto.id === id);
           return product;
         }
-        return null;
+        return null;        
       } catch (error) {
-        console.log(
-          `Error al leer el archivo en el método getById(): ${error}`
-        );
+        console.log(`Error al leer el archivo en el método getById(): ${error}`);
       }
     } else {
       console.log(`El id ${id} no es un número.`);
@@ -84,53 +69,42 @@ class Contenedor {
     try {
       let arrayProductos = await this.getAll();
       if (arrayProductos.length > 0) {
-        maxIndice = Math.max(...arrayProductos.map((producto) => producto.id));
+        maxIndice = Math.max(...arrayProductos.map(producto => producto.id))
       }
-      if (!isNaN(maxIndice)) {
-        arrayProductos.push({
-          title: producto.title,
-          price: producto.price,
-          thumbnail: producto.thumbnail,
-          id: ++maxIndice,
-        });
+      if(!isNaN(maxIndice)){
+        arrayProductos.push({title: producto.title, price: producto.price, thumbnail: producto.thumbnail, id: ++maxIndice}) 
         this.fs.writeFile(this.pathFile, JSON.stringify(arrayProductos));
         console.log(`Se agregó el id: ${maxIndice} al archivo`);
       } else {
-        console.log(
-          `No se pudo calcular el último id en el archivo, id: ${maxIndice}`
-        );
+        console.log(`No se pudo calcular el último id en el archivo, id: ${maxIndice}`);
       }
-      return maxIndice;
+      return (maxIndice);     
     } catch (error) {
-      console.log(
-        `Se produjo un error al intentar guardar el archivo: ${error}`
-      );
+      console.log(`Se produjo un error al intentar guardar el archivo: ${error}`);
     }
   }
-
-  async updateById(id, product) {
-    try {
-      const productos = await this.getAll();
-      const index = productos.findIndex((p) => p.id === id);
-      if (index < 0) {
-        throw new Error(`producto no encontrado`);
+    
+  async updateById(id, product){
+    try{
+        const productos = await this.getAll();
+        const index = productos.findIndex(p => p.id === id);
+        if (index < 0) {
+            throw new Error(`producto no encontrado`);
+        }
+        product.id = id; 
+        productos[index] = product;
+        await fs.writeFile(this.pathFile,JSON.stringify(productos));
+      } catch (error) {
+        console.log(`Se produjo un error al intentar actualizar el archivo: ${error}`);
       }
-      product.id = id; // ver si lo saco
-      productos[index] = product;
-      await fs.writeFile(this.pathFile, JSON.stringify(productos));
-    } catch (error) {
-      console.log(
-        `Se produjo un error al intentar actualizar el archivo: ${error}`
-      );
-    }
-  }
+}
 
   async deleteById(id) {
     try {
       let object = await this.getById(id);
-      if (object) {
+      if(object){
         let arrayProductos = await this.getAll();
-        let newArray = arrayProductos.filter((prod) => prod.id != id);
+        let newArray = arrayProductos.filter(prod => prod.id != id)
         this.fs.writeFile(this.pathFile, JSON.stringify(newArray));
         console.log(`Se ha eliminado el producto con id: ${id}`);
       } else {
@@ -147,8 +121,7 @@ class Contenedor {
       let cantidad = arrayProductos.length;
       if (arrayProductos.length > 0) {
         arrayProductos.length = 0;
-        // this.fs.writeFile(this.pathFile, JSON.stringify(arrayProductos));
-        this.fs.writeFile(this.pathFile, "");
+        this.fs.writeFile(this.pathFile, '');
         console.log(`Se han eliminado: ${cantidad} productos`);
       } else {
         console.log(`No existen productos en el archivo para eliminar.`);
